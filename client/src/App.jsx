012c1a8a9486e5adc2044e4321d7cb1b3bd703a3
HoangKhoi <thienkhoi0604@gmail.com>
2023-06-18 15:11:41 +0700

    setup and implement Navbar

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Footer, Navbar, Services, Transactions, Welcome } from "./components";
+
+export default function App() {
+  return (
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
+    </div>
+  );
+}
