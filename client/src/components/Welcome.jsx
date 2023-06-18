d675620cc635fdaee3891ed4dbe70d0ecf1a9951
HoangKhoi <thienkhoi0604@gmail.com>
2023-06-18 16:22:35 +0700

    fix layout Welcome

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -65,65 +65,63 @@ const Welcome = () => {
               Blockchain
             </div>
           </div>
-
-          <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
-            <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
-              <div className="flex justify-between flex-col w-full h-full">
-                <div className="flex justify-between items-start">
-                  <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
-                    <SiEthereum fontSize={21} color="#fff" />
-                  </div>
-                  <BsInfoCircle fontSize={17} color="#fff" />
-                </div>
-                <div>
-                  <p className="text-white font-light text-sm">Adrress</p>
-                  <p className="text-white font-semibold text-lg mt-1">
-                    Ethereum
-                  </p>
-                </div>
+        </div>
+      </div>
+      <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+        <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
+          <div className="flex justify-between flex-col w-full h-full">
+            <div className="flex justify-between items-start">
+              <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
+                <SiEthereum fontSize={21} color="#fff" />
               </div>
+              <BsInfoCircle fontSize={17} color="#fff" />
+            </div>
+            <div>
+              <p className="text-white font-light text-sm">Adrress</p>
+              <p className="text-white font-semibold text-lg mt-1">Ethereum</p>
             </div>
           </div>
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            <Input
-              placeholder="Address To"
-              name="addressTo"
-              type="text"
-              handleChange={() => {}}
-            />
-            <Input
-              placeholder="Amount (ETH)"
-              name="amount"
-              type="number"
-              handleChange={() => {}}
-            />
-            <Input
-              placeholder="Keyword (Gif)"
-              name="keyword"
-              type="text"
-              handleChange={() => {}}
-            />
-            <Input
-              placeholder="Enter Message"
-              name="message"
-              type="text"
-              handleChange={() => {}}
-            />
+        </div>
 
-            <div className="h-[1px] w-full bg-gray-400 my-2" />
+        <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
+          <Input
+            placeholder="Address To"
+            name="addressTo"
+            type="text"
+            handleChange={() => {}}
+          />
+          <Input
+            placeholder="Amount (ETH)"
+            name="amount"
+            type="number"
+            handleChange={() => {}}
+          />
+          <Input
+            placeholder="Keyword (Gif)"
+            name="keyword"
+            type="text"
+            handleChange={() => {}}
+          />
+          <Input
+            placeholder="Enter Message"
+            name="message"
+            type="text"
+            handleChange={() => {}}
+          />
 
-            {isLoading ? (
-              <Loader />
-            ) : (
-              <button
-                type="button"
-                onClick={handleSubmit}
-                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-              >
-                Send now
-              </button>
-            )}
-          </div>
+          <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+            >
+              Send now
+            </button>
+          )}
         </div>
       </div>
     </div>
