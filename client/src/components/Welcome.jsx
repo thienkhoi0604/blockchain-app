e94f4c602bd3232bbfaee9c4d524a6304b1b1109
HoangKhoi <thienkhoi0604@gmail.com>
2023-06-22 21:06:05 +0700

    add transaction

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 
 import { Loader } from "./";
+import { TransactionContext } from "../context/TransactionContext";
 
 const companyCommonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -20,12 +21,23 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  const connectWallet = () => {};
+  const {
+    currentAccount,
+    connectWallet,
+    handleChange,
+    sendTransaction,
+    formData,
+    isLoading,
+  } = useContext(TransactionContext);
 
   const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+
     e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
   };
 
   return (
@@ -40,14 +52,18 @@ const Welcome = () => {
             Krypto.
           </p>
 
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
-          >
-            <AiFillPlayCircle className="text-white mr-2" />
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              <AiFillPlayCircle className="text-white mr-2" />
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -88,25 +104,25 @@ const Welcome = () => {
             placeholder="Address To"
             name="addressTo"
             type="text"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
           <Input
             placeholder="Amount (ETH)"
             name="amount"
             type="number"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
           <Input
             placeholder="Keyword (Gif)"
             name="keyword"
             type="text"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
           <Input
             placeholder="Enter Message"
             name="message"
             type="text"
-            handleChange={() => {}}
+            handleChange={handleChange}
           />
 
           <div className="h-[1px] w-full bg-gray-400 my-2" />
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { TransactionsProvider } from "./context/TransactionContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <TransactionsProvider>
     <App />
-  </React.StrictMode>,
-)
+  </TransactionsProvider>
+);
